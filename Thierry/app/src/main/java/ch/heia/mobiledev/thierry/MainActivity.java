b42eb47e156dbb51c292ca3a082108a912147873
Patrick Audriaz <patrick.audriaz@edu.hefr.ch>
2019-03-31 22:29:40 +0200

    ..

--- Thierry/app/src/main/java/ch/heia/mobiledev/thierry/MainActivity.java
+++ Thierry/app/src/main/java/ch/heia/mobiledev/thierry/MainActivity.java
@@ -1,22 +1,38 @@
 package ch.heia.mobiledev.thierry;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+
+import com.google.android.material.appbar.CollapsingToolbarLayout;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
+import android.text.InputType;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    private String search = "";
+    Toolbar mActionBarToolbar;
+
+    // used for logging
+    private static final String TAG = "MainActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "---------------MainActicity---------------------");
+        Log.d(TAG, "onCreate called");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -46,6 +62,7 @@ public class MainActivity extends AppCompatActivity
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
+
         return true;
     }
 
@@ -56,13 +73,42 @@ public class MainActivity extends AppCompatActivity
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if (id == R.id.nav_bar_search) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Rechercher une ville");
+
+            // Set up the input
+            final EditText input = new EditText(this);
+            // Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
+            input.setInputType(InputType.TYPE_CLASS_TEXT);
+            builder.setView(input);
+
+            // Set up the buttons
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    search = input.getText().toString();
+                    Log.d(TAG, "----> " + search);
+                    mActionBarToolbar = (Toolbar) findViewById(R.id.toolbar);
+                    setSupportActionBar(mActionBarToolbar);
+                    getSupportActionBar().setTitle("My Title");
+
+                }
+            });
+            builder.setNegativeButton("Annuler", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.cancel();
+                }
+            });
+
+            builder.show();
         }
 
         return super.onOptionsItemSelected(item);
     }
+
+
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
@@ -71,7 +117,6 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_settings) {
-            // Handle the camera action
         } else if (id == R.id.nav_about) {
 
         }
@@ -79,5 +124,57 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        Log.d(TAG, "onSaveInstanceState called");
+        super.onSaveInstanceState(outState);
+        // outState.putInt("peerCounter", peerCount);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        Log.d(TAG, "onRestoreInstanceState called");
+        super.onRestoreInstanceState(savedInstanceState);
+        // peerCount = savedInstanceState.getInt("peerCounter");
+    }
+
+    // tests to visualize Android lifecycle, override all lifecycle callback methods
+    @Override
+    protected void onStart()  {
+        Log.d(TAG, "onStart called");
+        super.onStart();
+
+    }
+
+    @Override
+    protected void onResume()  {
+        Log.d(TAG, "onResume called");
+        super.onResume();
+
+    }
+
+    protected void onPause()  {
+        Log.d(TAG, "onPause called");
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop()  {
+        Log.d(TAG, "onStop called");
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy()  {
+        Log.d(TAG, "onDestroy called");
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onRestart()  {
+        Log.d(TAG, "onRestart called");
+        super.onRestart();
     }
 }
